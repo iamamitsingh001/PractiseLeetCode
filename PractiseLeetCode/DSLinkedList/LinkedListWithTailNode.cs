ea438168b4c8f98ad6ff1e242dc04b61acd3f9ea
iamamitsingh001 <iamamit.singh@hotmail.com>
2024-01-01 01:50:05 +0530

    test push

--- PractiseLeetCode/DSLinkedList/LinkedListWithTailNode.cs
+++ PractiseLeetCode/DSLinkedList/LinkedListWithTailNode.cs
@@ -24,6 +24,7 @@ namespace PractiseLeetCode.DSLinkedList
                 tail.Next = newItem;
                 tail = newItem;
             }
+
         }
         public void AddFirst(object data)
         {
