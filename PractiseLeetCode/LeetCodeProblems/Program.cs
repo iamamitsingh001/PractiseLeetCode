2a1ad55338e9f9e68a8329274dc93453ef3a19a7
iamamitsingh001 <iamamit.singh@hotmail.com>
2024-04-04 21:11:15 +0530

    New Implementation

---
+++ PractiseLeetCode/LeetCodeProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseLeetCode.LeetCodeProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TwoSumSolution twoSumSolution = new TwoSumSolution();
+            int[] request = { 3,2,4 };
+            var result = twoSumSolution.TwoSum(request, 6);
+        }
+    }
+}
---
+++ PractiseLeetCode/LeetCodeProblems/TwoSumSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseLeetCode.LeetCodeProblems
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            int diff = 0,i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                diff = target - nums[i];
+                if (diff == nums[j])
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+                if (j == nums.Length - 1)
+                {
+                    i++;j = i;
+                }
+            }
+            return result;
+        }
+    }
+}
